6172f0930f72617a10c0d3b28bea61be48a3a2b0
Lxg1c <teran_2005@mail.ru>
2025-02-11 15:30:22 +0300

    Added Search to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/App.scss'
 import {Header} from "./components/Header.tsx";
+import {Search} from "./components/Search.tsx";
 
 function App() {
 
   return (
       <div className='app container'>
           <Header />
+          <Search />
       </div>
   )
 }

7e42c97ca7066822f6e64ca58410bc38d37b9337
Lxg1c <teran_2005@mail.ru>
2025-02-11 19:12:37 +0300

    change type of export

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import "../styles/Header.scss"
 import React from 'react';
 
-export const Header: React.FC = () => {
+const Header: React.FC = () => {
     return (
         <header className='header'>
             <h1 id='header__title'>TODO LIST</h1>
         </header>
     )
 }
+
+export default Header;

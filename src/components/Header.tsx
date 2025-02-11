bfe5d9365acb8e5c1d34fa82ea1ee35930840fb4
Lxg1c <teran_2005@mail.ru>
2025-02-11 15:29:35 +0300

    Changed title text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Header: React.FC = () => {
     return (
         <header className='header'>
-            <h1 id='header__title'>TO DO LIST</h1>
+            <h1 id='header__title'>TODO LIST</h1>
         </header>
     )
 }

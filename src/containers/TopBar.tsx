37f2dbdef3e2926988ff60e3474c0887acd0b690
Lxg1c <teran_2005@mail.ru>
2025-02-12 13:25:07 +0300

    created top bar component

---
+++ src/containers/TopBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Search from "../components/Search.tsx";
+import CustomDropdown from "../components/Dropdown.tsx";
+import ChangeThemeBtn from "../components/ChangeThemeBtn.tsx";
+
+const TopBar: React.FC = () => {
+    return (
+        <div className='main__top flex mt-5 gap-4'>
+            <Search/>
+            <CustomDropdown/>
+            <ChangeThemeBtn/>
+        </div>
+    )
+}
+
+export default TopBar

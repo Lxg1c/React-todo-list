283c22e500b434640a56cab1b0297cef5c9bb0e9
Lxg1c <teran_2005@mail.ru>
2025-02-12 13:27:16 +0300

    Initialized Modal.tsx

---
+++ src/components/Modal.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import Search from "./Search.tsx";
+import '../styles/Modal.scss';
+import NewNoteControls from "../containers/NewNoteControls.tsx";
+
+const Modal: React.FC = () => {
+    const [portalRoot, setPortalRoot] = useState<HTMLElement | null>(null);
+
+    useEffect(() => {
+        const rootElement = document.getElementById("portal_root");
+        if (!rootElement) {
+            console.error("Portal root not found!");
+        } else {
+            setPortalRoot(rootElement);
+        }
+    }, []);
+
+    // Если portalRoot не найден, не рендерим компонент
+    if (!portalRoot) return null;
+
+    return ReactDOM.createPortal(
+        <div className="modal">
+            <div className="modal__content">
+                <h1 className="modal__content-title mb-6.25">NEW NOTE</h1>
+                <Search />
+                <NewNoteControls />
+            </div>
+        </div>,
+        portalRoot
+    );
+};
+
+export default Modal;

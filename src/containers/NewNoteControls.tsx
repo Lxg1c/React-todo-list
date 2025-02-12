607f323b1efc56857efd622326db91ba7fbb5717
Lxg1c <teran_2005@mail.ru>
2025-02-12 13:27:03 +0300

    Created container NewNoteControls.tsx

---
+++ src/containers/NewNoteControls.tsx
@@ -0,0 +1,15 @@
+import Btn from "../components/Btn.tsx";
+import {Button} from "antd";
+import React from "react";
+
+
+const NewNoteControls: React.FC = () => {
+    return (
+        <div className="modal__content-controls flex justify-between align-middle mt-32">
+            <Button>CANCEL</Button>
+            <Btn children='APPLY'/>
+        </div>
+    )
+}
+
+export default NewNoteControls;

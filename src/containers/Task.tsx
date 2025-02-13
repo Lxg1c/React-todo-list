b0c3eaae61455027deb0dc0a6d8d6e3589401b94
Lxg1c <teran_2005@mail.ru>
2025-02-13 17:20:39 +0300

    added onChange handler

--- src/containers/Task.tsx
+++ src/containers/Task.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import DeleteBtn from "../components/DeleteBtn.tsx";
 import ReEdit from "../components/ReEdit.tsx";
-import {Space} from 'antd';
-import type { CheckboxProps } from 'antd';
-import "../styles/Task.scss";
-import {useDispatch} from "react-redux";
-import {toggleTaskStatus} from "../store/slices/taskSlice.ts";
+import { Space } from 'antd';
+import { useDispatch } from "react-redux";
+import { toggleTaskStatus } from "../store/slices/taskSlice.ts";
 import TaskStatus from "../components/TaskStatus.tsx";
-
+import "../styles/Task.scss";
 
 interface TaskProps {
     id: number;
@@ -17,7 +15,11 @@ interface TaskProps {
 
 const Task: React.FC<TaskProps> = ({ id, taskContent, status }) => {
     const dispatch = useDispatch();
-    const onChange: CheckboxProps['onChange'] = () => dispatch(toggleTaskStatus(id))
+
+    // Обработчик для изменения состояния чекбокса
+    const onChange = () => {
+        dispatch(toggleTaskStatus(id)); // Обработчик событий, вызывающий экшен для изменения статуса задачи
+    };
 
     return (
         <>

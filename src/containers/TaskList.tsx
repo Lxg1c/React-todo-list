348e6d814eee8a734371186be00030c93efaac96
Lxg1c <teran_2005@mail.ru>
2025-02-13 15:16:30 +0300

    initialized TaskList container

---
+++ src/containers/TaskList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import emptyLight from '../../public/empty-light.svg'
+import Task from "../components/Task.tsx";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store.ts";
+import "../styles/TaskList.scss"
+
+const TaskList: React.FC = () => {
+    const tasks = useSelector((state: RootState) => state.task.tasks);
+
+    return (
+        <ul className="task__list">
+            {tasks.map((task, index: number) => (
+                <li key={index} className="task__list-item">
+                    <Task status={task.status} taskContent={task.taskContent} />
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default TaskList;

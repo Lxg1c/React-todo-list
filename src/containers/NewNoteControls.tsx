22e35f24048566ea67dfd5165ae8c27303393a66
Lxg1c <teran_2005@mail.ru>
2025-02-13 15:14:46 +0300

    Added annotation

--- src/containers/NewNoteControls.tsx
+++ src/containers/NewNoteControls.tsx
@@ -1,5 +1,5 @@
 import Btn from "../components/Btn.tsx";
-import {Button} from "antd";
+import { Button } from "antd";
 import React from "react";
 
 interface NewNoteControlsProps {
@@ -7,7 +7,13 @@ interface NewNoteControlsProps {
     onApply: () => void;
 }
 
-const NewNoteControls: React.FC = () => {
+const NewNoteControls: React.FC<NewNoteControlsProps> = ({ onClose, onApply }) => {
+    // Создаем новый обработчик, который вызывает оба действия
+    const handleApplyAndClose = () => {
+        onApply();
+        onClose();
+    };
+
     return (
         <div className="modal__content-controls flex justify-between align-middle mt-32">
             <Button>CANCEL</Button>

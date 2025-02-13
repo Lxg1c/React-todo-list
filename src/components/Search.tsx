9b121b417783a781a1c2f29661212052201684ab
Lxg1c <teran_2005@mail.ru>
2025-02-13 15:17:35 +0300

    Added interface and props to Input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { ConfigProvider, Input } from 'antd';
-import {CustomSearchTheme} from "../utils/const.ts";
+import { CustomSearchTheme } from "../utils/const.ts";
 
-const Search: React.FC = () => {
+interface SearchProps {
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search: React.FC<SearchProps> = ({ onChange }) => {
+    // Создаем состояние для хранения значения в поле ввода
     return (
-        <ConfigProvider
-            theme={CustomSearchTheme}>
-
-            <Input placeholder="Search note..." />
+        <ConfigProvider theme={CustomSearchTheme}>
+            <Input
+                placeholder="Search note..."
+                onChange={onChange}
+            />
         </ConfigProvider>
-    )
+    );
 };
 
 export default Search;

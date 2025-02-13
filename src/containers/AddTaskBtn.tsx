d9fe11abdc56ad359b30e0a3084f5416057a2eeb
Lxg1c <teran_2005@mail.ru>
2025-02-13 15:19:15 +0300

    Added props to Btn component

--- src/containers/AddTaskBtn.tsx
+++ src/containers/AddTaskBtn.tsx
@@ -2,10 +2,16 @@ import plusIcon from "../../public/plusIcon.svg"
 import React from 'react'
 import Btn from "../components/Btn.tsx";
 
-const AddTaskBtn: React.FC = () => {
+interface AddTaskBtnProps {
+    onClick: () => void;
+}
+
+const AddTaskBtn: React.FC<AddTaskBtnProps> = ({ onClick }) => {
     return (
-        <div className='max-w-12.5 fixed right-110 bottom-10'>
-            <Btn children={<img src={plusIcon} alt='Button to add Task'/>} />
+        <div className='max-w-12.5 fixed right-110 bottom-10' onClick={onClick}>
+            <Btn onClick={onClick}>
+                <img src={plusIcon} alt='Button to add Task'/>
+            </Btn>
         </div>
     )
 }

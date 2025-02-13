3bbc4353027e3d4a947155115b06f9c9e4cfb2e1
Lxg1c <teran_2005@mail.ru>
2025-02-13 17:16:00 +0300

    Changed icon from material ui

---
+++ src/components/ReEdit.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import IconButton from '@mui/material/IconButton';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
+
+const ReEdit: React.FC = () => {
+    return (
+        <IconButton aria-label="delete" size="large">
+            <CreateOutlinedIcon />
+        </IconButton>
+    )
+}
+
+export default ReEdit

7bf39e6de64b64578686992f4dbf02db8da7aa86
Lxg1c <teran_2005@mail.ru>
2025-02-14 20:14:06 +0300

    changed import

--- src/containers/TaskBar.tsx
+++ src/containers/TaskBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Search from "../components/Search.tsx";
+import Search from "../components/CustomInput.tsx";
 import CustomDropdown from "../components/Dropdown.tsx";
-import ChangeThemeBtn from "../components/ChangeThemeBtn.tsx";
+import ChangeThemeBtn from "./ChangeThemeBtn.tsx";
 
 const TaskBar: React.FC = () => {
     return (

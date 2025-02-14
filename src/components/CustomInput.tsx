bda574affa2b2a67c24018de8f0a5b621006b0d5
Lxg1c <teran_2005@mail.ru>
2025-02-14 19:51:19 +0300

    renamed from Search to CustomInput

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 import { ConfigProvider, Input } from 'antd';
 import { CustomSearchTheme } from "../utils/const.ts";
 
-interface SearchProps {
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
 
-const Search: React.FC<SearchProps> = ({ onChange }) => {
+const CustomInput: React.FC = () => {
     // Создаем состояние для хранения значения в поле ввода
     return (
         <ConfigProvider theme={CustomSearchTheme}>
             <Input
                 placeholder="Search note..."
-                onChange={onChange}
             />
         </ConfigProvider>
     );
 };
 
-export default Search;
+export default CustomInput;

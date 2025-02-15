d42a56b59b67e0189555166608307682a52203c6
Lxg1c <teran_2005@mail.ru>
2025-02-15 10:58:52 +0300

    used Custom hooks to get tasks and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,28 @@
-// App.tsx
 import './styles/App.scss';
 import Header from './containers/Header.tsx';
 import TaskBar from './containers/TaskBar.tsx';
 import AddTaskBtn from './containers/AddTaskBtn.tsx';
 import Modal from './containers/Modal.tsx';
-import React, {JSX, useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTask, setNewTaskInput } from './store/slices/taskSlice.ts';
+import {JSX, useState} from 'react';
+import { useSelector } from 'react-redux';
 import TaskList from './containers/TaskList.tsx';
 import {ConfigProvider} from "antd";
-import {selectCurrentTheme, selectNewTaskInput} from "./store/selectors.ts";
+import { selectCurrentTheme } from "./store/selectors.ts";
+import {useInfoFromCookie} from "./hooks/useInfoFromCookie.ts";
 
 function App(): JSX.Element {
+    // State to Modal view
     const [isModalVisible, setIsModalVisible] = useState(false);
-    const dispatch = useDispatch();
 
+    // Selectors
     const theme = useSelector(selectCurrentTheme);
-    const newTaskInput = useSelector(selectNewTaskInput);
+
+    // Custom hook to get theme and tasks from cookie
+    useInfoFromCookie()
 
     const openModal = () => setIsModalVisible(true);
     const closeModal = () => setIsModalVisible(false);
 
-    const onApply = () => {
-        if (newTaskInput.trim()) {
-            dispatch(
-                addTask({
-                    id: Date.now(), // Генерация уникального ID задачи
-                    taskContent: newTaskInput,
-                    status: false, // Задача не завершена по умолчанию
-                })
-            );
-        }
-    };
-
-    // Обработчик изменения значения в поле ввода
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(setNewTaskInput(e.target.value));
-    };
 
     return (
         <ConfigProvider
@@ -51,9 +37,6 @@ function App(): JSX.Element {
                 className="app"
                 style={{
                     background: theme === 'dark' ? '#252525' : '#ffffff',
-                    minHeight: '100vh', // Занимает минимум 100% высоты viewport
-                    display: 'flex',
-                    flexDirection: 'column',
                 }}
             >
                 <div className="app__container container">
@@ -64,7 +47,7 @@ function App(): JSX.Element {
                         <AddTaskBtn onClick={openModal} />
                     </main>
                     <div id="portal_root" className="right-0 left-0 bottom-0 top-0">
-                        {isModalVisible ? <Modal onClose={closeModal} onApply={onApply} onChange={handleChange} /> : null}
+                        {isModalVisible ? <Modal onClose={closeModal} /> : null}
                     </div>
                 </div>
             </div>

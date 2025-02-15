4236276e56a2d10f407c349b6f9e88403c21724f
Lxg1c <teran_2005@mail.ru>
2025-02-15 17:26:02 +0300

    Added value and placeholder prop

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -5,17 +5,20 @@ import {useSelector} from "react-redux";
 import {selectCurrentTheme} from "../store/selectors.ts";
 
 interface CustomInputProps {
-    onChange: React.ChangeEventHandler<HTMLInputElement>
+    onChange: React.ChangeEventHandler<HTMLInputElement>,
+    value?: string,
+    placeholder?: string,
 }
 
-const CustomInput: React.FC<CustomInputProps> = ({ onChange }) => {
+const CustomInput: React.FC<CustomInputProps> = ({ onChange, value, placeholder }) => {
     const theme = useSelector(selectCurrentTheme)
     // Создаем состояние для хранения значения в поле ввода
     return (
         <ConfigProvider theme={theme === 'light' ? CustomSearchTheme : CustomSearchDarkTheme}>
             <Input
+                value={value}
                 onChange={onChange}
-                placeholder="Search note..."
+                placeholder={placeholder ? placeholder : "Search note..."}
             />
         </ConfigProvider>
     );

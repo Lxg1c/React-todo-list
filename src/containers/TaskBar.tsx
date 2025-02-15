48d8154d4c32cd3c37a1d6dfbd29b50edfedbd42
Lxg1c <teran_2005@mail.ru>
2025-02-15 17:24:20 +0300

    created and added props to Search

--- src/containers/TaskBar.tsx
+++ src/containers/TaskBar.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Search from "../components/CustomInput.tsx";
 import CustomDropdown from "../components/Dropdown.tsx";
 import ChangeThemeBtn from "./ChangeThemeBtn.tsx";
+import {useDispatch} from "react-redux";
+import { setSearchQuery } from '../store/slices/taskSlice.ts';
 
 const TaskBar: React.FC = () => {
+    const dispatch = useDispatch();
+    const [search, setSearch] = useState<string>('');
+
+    // Функция для обновления поискового запроса
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const query = e.target.value;
+        setSearch(query);
+        dispatch(setSearchQuery(query));  // Обновляем поиск в Redux
+    };
+
     return (
         <div className='main__top flex mt-5 gap-4'>
-            <Search/>
+            <Search value={search} onChange={onSearchChange} />
             <CustomDropdown/>
             <ChangeThemeBtn/>
         </div>

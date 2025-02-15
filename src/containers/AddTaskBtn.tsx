d8aefa9b0264e8af3df99eaff1947bfe5fa9edb4
Lxg1c <teran_2005@mail.ru>
2025-02-15 11:00:14 +0300

    changed btn to floatBtn

--- src/containers/AddTaskBtn.tsx
+++ src/containers/AddTaskBtn.tsx
@@ -1,6 +1,6 @@
 import plusIcon from "../assets/plusIcon.svg"
 import React from 'react'
-import Btn from "../components/Btn.tsx";
+import FloatBtn from "../components/FloatBtn.tsx";
 
 interface AddTaskBtnProps {
     onClick: () => void;
@@ -9,9 +9,9 @@ interface AddTaskBtnProps {
 const AddTaskBtn: React.FC<AddTaskBtnProps> = ({ onClick }) => {
     return (
         <div className='max-w-12.5 fixed right-110 bottom-10' onClick={onClick}>
-            <Btn onClick={onClick}>
+            <FloatBtn onClick={onClick}>
                 <img src={plusIcon} alt='Button to add Task'/>
-            </Btn>
+            </FloatBtn>
         </div>
     )
 }

ee8a2beca11873ba18973ac82e4e15e1937467cb
Lxg1c <teran_2005@mail.ru>
2025-02-15 17:25:43 +0300

    Added size btn prop

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,16 +1,18 @@
 import React, { ReactNode } from 'react'
 import { Button, ConfigProvider } from "antd";
 import { CustomBtn } from "../utils/const.ts";
+import {SizeType} from "antd/es/config-provider/SizeContext";
 
 interface BtnProps {
     children: ReactNode;
     onClick: () => void;
+    size?: SizeType
 }
 
-const Btn: React.FC<BtnProps> = ({ children, onClick}) => {
+const Btn: React.FC<BtnProps> = ({ children, onClick, size}) => {
     return (
         <ConfigProvider theme={CustomBtn}>
-            <Button size='large' onClick={onClick}>
+            <Button size={size ? size : 'large'} onClick={onClick}>
                 {children}
             </Button>
         </ConfigProvider>
